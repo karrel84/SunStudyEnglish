7a25e2e509d61d985409e2779fb2511d8d56df09
karrel84@naver.com <dlwnqkf1>
2017-07-07 18:46:03 +0900

    get the word 와 연동하자

--- app/src/main/java/com/karrel/sunstudyenglish/main/MainActivity.java
+++ app/src/main/java/com/karrel/sunstudyenglish/main/MainActivity.java
@@ -1,59 +1,138 @@
 package com.karrel.sunstudyenglish.main;
 
-import android.content.Intent;
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.util.Log;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.karrel.sunstudyenglish.R;
-import com.karrel.sunstudyenglish.base.BaseActivity;
-import com.karrel.sunstudyenglish.databinding.ActivityMainBinding;
-import com.kerrel.getthewordlibrary.OcrCaptureActivity;
+import com.karrel.sunstudyenglish.main.gettheword.FragmentGetWord;
+import com.karrel.sunstudyenglish.main.wordbook.FragmentWordBook;
 
-/**
- * Created by 이주영 on 2017-06-16.
- */
-
-public class MainActivity extends BaseActivity {
-
-    private ActivityMainBinding mBinding;
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
+        // 타이틀을 세팅한다
+        Toolbar toolbar = setupTitle();
+
+        // 좌측 슬라이드 메뉴를 세팅하자
+        setupLeftDrawer(toolbar);
+
     }
 
+    /**
+     * 뒤로가기
+     */
     @Override
-    protected void onLoadOnce() {
-        super.onLoadOnce();
-
-        // 버튼 세팅
-        setupButtons();
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
-    private void setupButtons() {
-        Log.d("TAG", "setupButtons");
-        mBinding.getWord.setOnClickListener(v -> {
-            Log.d("TAG", "getWord");
-            grubUpWord();
-        });
+    /**
+     * 옵션메뉴를 만든다.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
-    // 단어를 캐자
-    private void grubUpWord() {
-        Intent intent = new Intent(this, OcrCaptureActivity.class);
-        startActivity(intent);
+    /**
+     * 타이틀바 메뉴 옵션
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
-    // 단어를 변환 하자
-    private void englishToKorean() {
+    /**
+     * 테비게이션아이템이 선택되면 이 메소드가 호출된다.
+     *
+     * @param item
+     * @return
+     */
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
 
+        Fragment fragment = null;
+        if (id == R.id.nav_wordbook) {
+            fragment = new FragmentWordBook();
+        } else if (id == R.id.nav_getword) {
+            fragment = new FragmentGetWord();
+        }
+        if (fragment != null) {
+            changeFragment(fragment);
+        }
+
+        closeDrawer();
+        return true;
     }
 
-    // 서버에 저장하자
-    private void saveToServer() {
+    /**
+     * 좌측 메뉴 닫자
+     */
+    private void closeDrawer() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+    }
 
+    /**
+     * 좌측 슬라이드 메뉴를 세팅하자
+     *
+     * @param toolbar
+     */
+    private void setupLeftDrawer(Toolbar toolbar) {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    /**
+     * 타이틀을 세팅한다
+     */
+    private Toolbar setupTitle() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        return toolbar;
+    }
+
+    public void changeFragment(Fragment fragment) {
+        // 최초 플래그먼트를 설정한다.
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment, fragment);
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/karrel/sunstudyenglish/main/wordbook/FragmentWordBook.java
@@ -0,0 +1,38 @@
+package com.karrel.sunstudyenglish.main.wordbook;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.karrel.sunstudyenglish.R;
+
+public class FragmentWordBook extends Fragment {
+
+    public FragmentWordBook() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_fragment_word_book, container, false);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
