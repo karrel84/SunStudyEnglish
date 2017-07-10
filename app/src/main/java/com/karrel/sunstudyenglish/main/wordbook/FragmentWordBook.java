2f6a36e0cf220c20f32fec90eb0fb02df1455ead
karrel84@naver.com <dlwnqkf1>
2017-07-10 18:54:58 +0900

    get the word 와 연동하자

--- app/src/main/java/com/karrel/sunstudyenglish/main/wordbook/FragmentWordBook.java
+++ app/src/main/java/com/karrel/sunstudyenglish/main/wordbook/FragmentWordBook.java
@@ -1,15 +1,21 @@
 package com.karrel.sunstudyenglish.main.wordbook;
 
 import android.content.Context;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.karrel.sunstudyenglish.FireBaseActivity;
 import com.karrel.sunstudyenglish.R;
+import com.karrel.sunstudyenglish.databinding.FragmentFragmentWordBookBinding;
 
 public class FragmentWordBook extends Fragment {
+
+    private FragmentFragmentWordBookBinding mBinding;
 
     public FragmentWordBook() {
         // Required empty public constructor
@@ -21,9 +27,11 @@ public class FragmentWordBook extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_fragment_word_book, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_fragment_word_book, container, false);
+        // 파이어베이스 액티비티로 이동
+        mBinding.firebase.setOnClickListener(v -> startActivity(new Intent(getContext(), FireBaseActivity.class)));
+        return mBinding.getRoot();
     }
 
     @Override
