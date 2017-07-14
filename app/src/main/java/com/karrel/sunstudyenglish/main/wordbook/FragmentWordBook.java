e42fc78cc57323a3ae3bef073b007680ccf406ad
karrel84@naver.com <dlwnqkf1>
2017-07-14 18:14:21 +0900

    단어 수집

--- app/src/main/java/com/karrel/sunstudyenglish/main/wordbook/FragmentWordBook.java
+++ app/src/main/java/com/karrel/sunstudyenglish/main/wordbook/FragmentWordBook.java
@@ -1,20 +1,24 @@
 package com.karrel.sunstudyenglish.main.wordbook;
 
-import android.content.Context;
-import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.karrel.sunstudyenglish.FireBaseActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.karrel.sunstudyenglish.R;
 import com.karrel.sunstudyenglish.databinding.FragmentFragmentWordBookBinding;
 
 public class FragmentWordBook extends Fragment {
 
+    private final String TAG = "FragmentWordBook";
     private FragmentFragmentWordBookBinding mBinding;
 
     public FragmentWordBook() {
@@ -30,17 +34,62 @@ public class FragmentWordBook extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_fragment_word_book, container, false);
         // 파이어베이스 액티비티로 이동
-        mBinding.firebase.setOnClickListener(v -> startActivity(new Intent(getContext(), FireBaseActivity.class)));
+        mBinding.firebase.setOnClickListener(v -> testFireBase());
         return mBinding.getRoot();
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-    }
+    /**
+     * //     * 파이어베이스를 테스트 하자
+     * //
+     */
+    private void testFireBase() {
+        Log.d(TAG, ":: testFireBase");
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("my message!! ");
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
+
+        myRef.child("users").child("1234").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d(TAG, ":: onDataChange");
+                Object obj = dataSnapshot.getValue();
+                if (obj != null) {
+                    Log.d(TAG, "obj -> " + obj.toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d(TAG, ":: onDataChange");
+                Object obj = dataSnapshot.getValue();
+                Log.d(TAG, "obj -> " + obj.toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+//                dataSnapshot.getValue(String.class)
+//                Log.d(TAG, "value -> " + dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "Failed to read value.", databaseError.toException());
+            }
+        });
     }
 }
