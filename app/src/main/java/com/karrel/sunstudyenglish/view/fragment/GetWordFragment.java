87fd2599fb1c3a286864f34602cf9c1a2ea59b18
karrel <karrel84@naver.com>
2017-07-27 18:19:35 +0900

    단어장  > 저장한 단어 그룹노출
    단어기억 화면
     > 단어기억 화면 출력

--- app/src/main/java/com/karrel/sunstudyenglish/view/fragment/GetWordFragment.java
+++ app/src/main/java/com/karrel/sunstudyenglish/view/fragment/GetWordFragment.java
@@ -94,88 +94,6 @@ public class GetWordFragment extends BaseFragment {
 
             }
         });
-
-        Log.e(TAG, " mReference.getKey() > " + mReference.child("posts").getKey());
-
-        // TODO test code
-        mBinding.addUser.setOnClickListener(v -> writeNewUser("karrel", "이주영", "karrel84@naver.com"));
-        // TODO test code
-        // 이름을 변경한다
-        mBinding.updateName.setOnClickListener(v -> mReference.child("users").child("karrel").child("username").setValue("박지훈"));
-        // TODO test code
-        // 데이터추가
-        mBinding.addDataList.setOnClickListener(v -> writeNewPost("karrel", "이주영", "타이틀입니다.", "내용입니다."));
-        // TODO test
-        // 데이터 삭제
-        mBinding.deleteData.setOnClickListener(v -> mReference.child("posts").removeValue());
-
-        // TODO test
-        mBinding.transactionSave.setOnClickListener(v -> onStarClicked(mReference));
-    }
-
-    // TODO test
-    private void onStarClicked(DatabaseReference postRef) {
-        postRef.runTransaction(new Transaction.Handler() {
-            @Override
-            public Transaction.Result doTransaction(MutableData mutableData) {
-                Post p = mutableData.getValue(Post.class);
-                if (p == null) {
-                    return Transaction.success(mutableData);
-                }
-
-                if (p.stars.containsKey(getUid())) {
-                    // Unstar the post and remove self from stars
-                    p.starCount = p.starCount - 1;
-                    p.stars.remove(getUid());
-                } else {
-                    // Star the post and add self to stars
-                    p.starCount = p.starCount + 1;
-                    p.stars.put(getUid(), true);
-                }
-
-                // Set value and report transaction success
-                mutableData.setValue(p);
-                return Transaction.success(mutableData);
-            }
-
-            @Override
-            public void onComplete(DatabaseError databaseError, boolean b,
-                                   DataSnapshot dataSnapshot) {
-                // Transaction completed
-                Log.d(TAG, "postTransaction:onComplete:" + databaseError);
-            }
-        });
-    }
-
-    // TODO testCode
-
-    /**
-     * 이런거는 한번만 호출해서 JSON트리의 여러위치에 동시업데이트하기위한 목적이다.
-     */
-    private void writeNewPost(String userId, String username, String title, String body) {
-        // posts라는 차일드를 만들고 키를 가져온다.
-        String key = mReference.child("posts").push().getKey();
-        // post객체를 만든다.
-        Post post = new Post(userId, username, title, body);
-        // 만들어진 객체를 맵으로 변환
-        Map<String, Object> postValues = post.toMap();
-
-        // 여러개의 객체를 한번에 넣으려고할때 사용할 맵을 만든다.
-        Map<String, Object> childUpdates = new HashMap<>();
-        // 맵에 케이스1의 데이터를 만든다
-        childUpdates.put("/posts/" + key, postValues);
-        // 맵에 케이스2의 데이터를 만든다
-        childUpdates.put("/user-posts/" + userId + "/" + key, postValues);
-
-        // 여러개의 객체를 업데이트 한다.
-        mReference.updateChildren(childUpdates);
-    }
-
-    // TODO test code
-    private void writeNewUser(String userId, String name, String email) {
-        User user = new User(name, email);
-
-        mReference.child("users").child(userId).setValue(user);
     }
 
     @Override
@@ -228,12 +146,12 @@ public class GetWordFragment extends BaseFragment {
 
         @Override
         public void onCompleted(ArrayList<WordItem> wordItems) {
-            // todo 검색완료가 되었습니다 리스트를 받아가세요.
+            // 검색완료가 되었습니다 리스트를 받아가세요.
             saveListToServer(wordItems);
         }
     };
 
-    // todo 서버에 리스트를 저장해볼까요
+    // 서버에 리스트를 저장해볼까요
     private void saveListToServer(ArrayList<WordItem> wordItems) {
 
         StringBuilder builder = new StringBuilder();
@@ -247,11 +165,6 @@ public class GetWordFragment extends BaseFragment {
 
     private void addWord(WordItem item) {
         mReference.child("words").child(item.word).setValue(item);
-//        mReference.child("users").child("karrel84").setValue(item.word);
-    }
-
-    public String getUid() {
-        return "karrel8410";
     }
 
     public String getTime() {
