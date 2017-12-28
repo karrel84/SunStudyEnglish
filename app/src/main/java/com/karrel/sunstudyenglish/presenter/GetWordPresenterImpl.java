b363b05624e657cfc13bdbfd920ac7e85bf0ec5b
karrel <karrel84@naver.com>
2017-12-28 10:54:03 +0900

    단어 수집  - 단어 가져오는 속도 개선

--- app/src/main/java/com/karrel/sunstudyenglish/presenter/GetWordPresenterImpl.java
+++ app/src/main/java/com/karrel/sunstudyenglish/presenter/GetWordPresenterImpl.java
@@ -1,22 +1,30 @@
 package com.karrel.sunstudyenglish.presenter;
 
+import android.support.v4.util.Pair;
 import android.text.TextUtils;
 import android.util.Log;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.karrel.mylibrary.RLog;
 import com.karrel.sunstudyenglish.model.WordItem;
 import com.karrel.sunstudyenglish.base.util.EnglishToKorean;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Queue;
 import java.util.regex.Pattern;
 
 import rx.Observable;
+import rx.Observer;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
 import rx.schedulers.Schedulers;
+import rx.subjects.PublishSubject;
 
 /**
  * Created by rell on 2017-07-14.
@@ -53,30 +61,62 @@ public class GetWordPresenterImpl implements GetWordPresenter {
     public void onActivityResult(ArrayList<String> wordsList) {
         Log.d(TAG, wordsList.toString());
 
-        mView.showProgress();
+//        mView.showProgress();
         mWordItems.clear();
 
-        getWord(wordsList)
-                .subscribeOn(Schedulers.io()) // 작업은 비동기 스레드에서
-                .observeOn(AndroidSchedulers.mainThread()) // UI작업은 메인스레드에서
-                .subscribe(new Subscriber<WordItem>() {
-                    @Override
-                    public void onCompleted() {
-                        mView.hideProgress();
-//                        mView.onCompleted(mWordItems);
-                    }
+        // 큐에 넣고 작업하는건 어떨까?
+        Queue<List<String>> queue = new ArrayDeque<>();
+        queue.add(wordsList);
 
-                    @Override
-                    public void onError(Throwable e) {
-                    }
 
-                    @Override
-                    public void onNext(WordItem wordItem) {
-                        mView.addWordItem(wordItem);
-                        addWordItem(wordItem);
-                        mWordItems.add(wordItem);
-                    }
-                });
+        // 최대 사이즈의 값이 2의 4승보다 작으면은 스레드 16개를 돌릴필요가 없으므로 최대 2의 제곱만큼 돌린다.
+        int size = (int) (Math.log(wordsList.size()) / Math.log(2));
+        RLog.e("size : " + size);
+        if (size > 4) size = 4;
+
+        // 2의 제곱만큼 돌린다. 그러면 큐에 최종 4개가 담겨야한다.
+        for (int j = 0; j < size; j++) {
+            // 루프를 돌기전에 사이즈를 확인한다 for문에서 확인하면 사이즈가 변하기 때문
+            final int queueSize = queue.size();
+            for (int i = 0; i < queueSize; i++) {
+                List<String> list = queue.poll();
+                Pair<List<String>, List<String>> pair = halfList(list);
+                // 담은 리스트를 루프를 돌려서 다시 반으로 담아서 어딘가에 담는다.
+                queue.add(pair.first);
+                queue.add(pair.second);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            getWord(queue.poll())
+                    .subscribeOn(Schedulers.io()) // 작업은 비동기 스레드에서
+                    .observeOn(AndroidSchedulers.mainThread()) // UI작업은 메인스레드에서
+                    .subscribe(new Subscriber<WordItem>() {
+                        @Override
+                        public void onCompleted() {
+//                        mView.hideProgress();
+                        }
+
+                        @Override
+                        public void onError(Throwable e) {
+                        }
+
+                        @Override
+                        public void onNext(WordItem wordItem) {
+                            mView.addWordItem(wordItem);
+                            addWordItem(wordItem);
+                            mWordItems.add(wordItem);
+                        }
+                    });
+        }
+    }
+
+    public Pair<List<String>, List<String>> halfList(List<String> list) {
+        System.out.println("called halfList");
+        int size = list.size();
+        int halfCnt = size / 2;
+
+        return Pair.create(list.subList(0, halfCnt), list.subList(halfCnt, size));
     }
 
     private void addWordItem(WordItem item) {
@@ -117,7 +157,7 @@ public class GetWordPresenterImpl implements GetWordPresenter {
         mReference.child("users").child("karrel").child("words").child(getTime()).setValue(builder.toString());
     }
 
-    private Observable<WordItem> getWord(ArrayList<String> wordsList) {
+    private Observable<WordItem> getWord(List<String> wordsList) {
         return Observable.from(wordsList)
                 .filter(s -> !TextUtils.isEmpty(s)) // 빈값인지 판단
                 .filter(s -> regularExpression(s)) // 문자인지 판단
@@ -128,6 +168,52 @@ public class GetWordPresenterImpl implements GetWordPresenter {
                     else return new WordItem(s, wordMeans);
                 }) // 문자의 뜻을 가져옴
                 .filter(i -> i != null);
+    }
+
+    /**
+     * 단어를 병렬처리한다.
+     *
+     * @param wordsList
+     * @return
+     */
+    private PublishSubject<WordItem> getWordMuliplex(ArrayList<String> wordsList) {
+
+        PublishSubject<WordItem> subject = PublishSubject.create();
+
+        for (String word : wordsList) {
+            RLog.d("word : " + word);
+            Observable.just(word)
+                    .subscribeOn(Schedulers.io()) // 작업은 비동기 스레드에서
+                    .observeOn(AndroidSchedulers.mainThread()) // UI작업은 메인스레드에서
+                    .filter(s -> !TextUtils.isEmpty(s)) // 빈값인지 판단
+                    .filter(s -> regularExpression(s)) // 문자인지 판단
+                    .map(s ->
+                    {
+                        String wordMeans = mEnglishToKorean.getWord(s);
+                        if (TextUtils.isEmpty(wordMeans.trim())) return null;
+                        else return new WordItem(s, wordMeans);
+                    }) // 문자의 뜻을 가져옴
+                    .filter(i -> i != null)
+                    .subscribe(new Observer<WordItem>() {
+                        @Override
+                        public void onCompleted() {
+                            RLog.d("onCompleted");
+                        }
+
+                        @Override
+                        public void onError(Throwable e) {
+                            RLog.e(e.getMessage());
+                        }
+
+                        @Override
+                        public void onNext(WordItem wordItem) {
+                            RLog.d("onNext : " + wordItem.toString());
+                            subject.onNext(wordItem);
+                        }
+                    })
+            ;
+        }
+        return subject;
     }
 
     /**
--- app/src/test/java/com/karrel/sunstudyenglish/ExampleUnitTest.java
+++ app/src/test/java/com/karrel/sunstudyenglish/ExampleUnitTest.java
@@ -1,6 +1,13 @@
 package com.karrel.sunstudyenglish;
 
+import android.support.v4.util.Pair;
+
 import org.junit.Test;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 
 import static org.junit.Assert.*;
 
@@ -14,4 +21,65 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void pairTest() {
+        System.out.println("pairTest");
+    }
+
+    @Test
+    public void subLists() {
+        System.out.println("subLists");
+        List<String> regacyList = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            regacyList.add(i + " 번째 데이터입니다.");
+        }
+
+        // 큐에 넣고 작업하는건 어떨까?
+        Queue<List<String>> queue = new ArrayDeque<>();
+        queue.add(regacyList);
+
+        // 2의 제곱만큼 돌린다. 그러면 큐에 최종 4개가 담겨야한다.
+        for (int j = 0; j < 3; j++) {
+            // 루프를 돌기전에 사이즈를 확인한다 for문에서 확인하면 사이즈가 변하기 때문
+            final int queueSize = queue.size();
+            for (int i = 0; i < queueSize; i++) {
+                List<String> list = queue.poll();
+                Pair<List<String>, List<String>> pair = halfList(list);
+                // 담은 리스트를 루프를 돌려서 다시 반으로 담아서 어딘가에 담는다.
+                queue.add(pair.first);
+                queue.add(pair.second);
+            }
+        }
+
+        int idx = 0;
+        while (!queue.isEmpty()) {
+            idx++;
+
+            List<String> list = queue.poll();
+            for (String str : list) {
+                System.out.println(String.format("%s번 리스트 :: %s", idx, str));
+            }
+        }
+    }
+
+    public Pair<List<String>, List<String>> halfList(List<String> list) {
+        System.out.println("called halfList");
+        int size = list.size();
+        int halfCnt = size / 2;
+
+        return Pair.create(list.subList(0, halfCnt), list.subList(halfCnt, size));
+    }
+
+    @Test
+    public void testLog(){
+        int x = 1200;
+
+        System.out.println(logB(x, 2));
+    }
+
+    public static double logB(double x, double base) {
+        return Math.log(x) / Math.log(base);
+    }
+
 }
